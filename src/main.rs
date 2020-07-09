bad5eb5b66be7d57b6c858a416c4dd2ea137b107
Ryoh Kawai <kawairyoh@gmail.com>
2020-07-09 20:56:29 +0900

    Add imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use {
+    hyper::{
+        service::{make_service_fn, service_fn},
+        Body, Client, Request, Response, Server, Uri,
+    },
+    std::net::SocketAddr,
+};
+
 fn main() {
     println!("Hello, world!");
 }
