62e575eb26bfe0240fdd4182df469ba8cb800a95
Ryoh Kawai <kawairyoh@gmail.com>
2022-10-02 18:09:29 +0000

    Reset code sample

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,27 @@
-use {
-    hyper::{
-        service::{make_service_fn, service_fn},
-        Body, Request, Response, Server, StatusCode,
-    },
-    std::net::SocketAddr,
-    tokio::fs::File,
-    tokio_util::codec::{BytesCodec, FramedRead},
-};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 
-static NOTFOUND: &[u8] = b"Not Found";
-
-/// HTTP status code 404
-fn not_found() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(NOTFOUND.into())
-        .unwrap()
-}
-
-async fn serve_req(_req: Request<Body>) -> anyhow::Result<Response<Body>> {
-    println!("Got headers at {:?}", _req.headers());
-
-    if let Ok(file) = File::open("../tests/test.html").await {
-        let stream = FramedRead::new(file, BytesCodec::new());
-        let body = Body::wrap_stream(stream);
-        return Ok(Response::new(body));
-    }
-
-    Ok(not_found())
-}
-
-async fn run_server(addr: SocketAddr) {
-    println!("Listening on http://{}", addr);
-
-    let serve_future = Server::bind(&addr).serve(make_service_fn(|_| async {
-        Ok::<_, hyper::Error>(service_fn(serve_req))
-    }));
-
-    if let Err(e) = serve_future.await {
-        eprintln!("server error: {}", e);
-    }
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello World")))
 }
 
 #[tokio::main]
 async fn main() {
+    // Construct our SocketAddr to listen on...
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    run_server(addr).await;
+    // And a MakeService to handle each connection...
+    let make_service = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle))
+    });
+
+    // Then bind and serve...
+    let server = Server::bind(&addr).serve(make_service);
+
+    // And run forever...
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
